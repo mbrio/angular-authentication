9a82aca086bfd50a20160a08064cb95c8d22a33e
Michael Diolosa <michael.diolosa@gmail.com>
2013-03-20 20:48:46 -0400

    Built out an example app

--- public/index.js
+++ public/index.js
@@ -3,22 +3,119 @@
 (function () {
   'use strict';
 
+  var apiAuthUrl = 'api/auth',
+    _authenticated = false,
+    _username = null,
+    _id = null;
+
   angular
-    .module('authtest', ['http-auth-interceptor', 'authentication'])
+    .module('authtest', ['http-auth-interceptor', 'authentication', 'ngMockE2E'])
+
+    // Mock the server responses
+    .run(['$httpBackend', function ($httpBackend) {
+      $httpBackend.whenGET(apiAuthUrl).respond(function (method, url, data) {
+        if (_authenticated === true) {
+          return [200, {username: _username, id: _id}];
+        }
+
+        return [401];
+      });
+
+      $httpBackend.whenPOST(apiAuthUrl).respond(function (method, url, data) {
+        data = angular.fromJson(data);
+
+        if (data.username && data.password) {
+          _authenticated = true;
+          _username = data.username;
+          _id = Math.round(Math.random() * 1000 + 1);
+          return [200, {username: _username, id: _id, success: true}];
+        }
+
+        _authenticated = false;
+        return [401, {success: false}];
+      });
+
+      $httpBackend.whenDELETE(apiAuthUrl).respond(function (method, url, data) {
+        _authenticated = false;
+        return [200];
+      });
+    }])
+
+    // Configure my authority provider with a URL handler
     .config(['authServiceProvider', 'authorityProvider',
       function (authServiceProvider, authorityProvider) {
         authServiceProvider.addUrlHandler(function (response, deferred) {
-          var canHandle = response.config.url === 'api/auth';
+          var canHandle = response.config.method === 'POST' &&
+            response.config.url === apiAuthUrl;
 
           if (canHandle) { deferred.reject(response); }
           return canHandle;
         });
       }])
-    .controller('signin', ['$scope', 'principal',
-      function ($scope, principal) {
+
+    .controller('signin', ['$http', '$rootScope', '$scope', 'principal', 'authority',
+      function ($http, $rootScope, $scope, principal, authority) {
         $scope.user = principal;
-        $scope.signinClick = function () {
-          console.log('test');
+
+        $scope.username = '';
+        $scope.password = '';
+        $scope.message = null;
+
+        $http.get(apiAuthUrl).success(function (data) {
+          console.log('received user data: ');
+          console.dir(data);
+        });
+
+        // sign in button handler
+        $scope.signinButtonClick = function () {
+          var body = {username: $scope.username, password: $scope.password};
+
+          $scope.message = null;
+
+          $http
+            .post(apiAuthUrl, body)
+            .success(function (data) {
+              authority.authorize(data);
+              $scope.username = '';
+              $scope.password = '';
+            })
+            .error(function () {
+              $scope.message = 'Incorrect credentials';
+              $rootScope.$broadcast('event:signin-failed');
+            });
         };
+
+        // sign out button handler
+        $scope.signoutButtonClick = function () {
+          $scope.message = '';
+
+          $http
+            .delete(apiAuthUrl)
+            .success(function () {
+              authority.deauthorize();
+            });
+        };
+
+        // These events are fired by the authService provider, they're pretty
+        // self explanatory
+        $scope.$on('event:auth-loginRequired', function () {
+          console.log('need login');
+        });
+
+        $scope.$on('event:auth-loginConfirmed', function () {
+          console.log('logged in');
+        });
+
+        $scope.$on('event:signin-failed', function () {
+          console.log('incorrect credentials');
+        });
+
+        $scope.$on('event:authority-authorized', function () {
+          console.log('authorized');
+        });
+
+        $scope.$on('event:authority-deauthorized', function () {
+          console.log('deauthorized');
+        });
       }]);
 }());
