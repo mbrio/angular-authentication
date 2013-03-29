9240c15298bb4a8b0d5a03316b1e5acf4370c1f5
Michael Diolosa <michael.diolosa@gmail.com>
2013-03-28 20:44:48 -0400

    Work on travis support

--- config/karma-e2e.conf.js
+++ config/karma-e2e.conf.js
@@ -55,7 +55,7 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Chrome'];
+browsers = ['PhantomJS'];
 
 
 // If browser does not capture in given timeout [ms], kill it
